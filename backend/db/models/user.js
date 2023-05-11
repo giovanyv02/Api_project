ea2a3d7c555018e9654408b2e74807d22d0c69aa
giovany <victorgiovany02@gmail.com>
2023-05-11 16:25:18 -0400

    Add User model scope for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       // define association here
     }
@@ -45,6 +41,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope:{
+      attributes:{
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
   });
   return User;
 };
