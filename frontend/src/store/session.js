87ba9311a0e8bf7f3a7b082f3b4015e6de728975
giovany <victorgiovany02@gmail.com>
2023-06-04 11:47:52 -0400

    logOut

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -55,6 +55,14 @@ export const signup = (user) => async (dispatch) => {
 };
 // ...
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
